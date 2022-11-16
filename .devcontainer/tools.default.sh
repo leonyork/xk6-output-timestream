a5fad7a0a5746e6b234ff1e289054dbff055eba0
Leon York <leon@leonyork.com>
2022-11-16 20:57:53 +0000

    chore: update github cli version

--- .devcontainer/tools.default.sh
+++ .devcontainer/tools.default.sh
@@ -65,7 +65,7 @@ chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" |
 	tee /etc/apt/sources.list.d/github-cli.list >/dev/null
 apt update
-apt install gh=2.20.0 -y
+apt install gh=2.20.2 -y
 
 # Setup anything we need for development
 make init-dev
