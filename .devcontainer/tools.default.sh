26102994d87b49d59d506d0083b1a96b3aeb7806
Leon York <leon@leonyork.com>
2023-01-12 22:59:56 +0000

    ci: no need to specify github tool version

--- .devcontainer/tools.default.sh
+++ .devcontainer/tools.default.sh
@@ -52,7 +52,7 @@ chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" |
 	tee /etc/apt/sources.list.d/github-cli.list >/dev/null
 apt update
-apt install gh=2.20.2 -y
+apt install gh -y
 
 # Setup anything we need for development
 make init-dev
